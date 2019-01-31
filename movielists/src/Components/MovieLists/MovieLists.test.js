d9665eaca6dbd2eb6f4a0e3778b34e02d0df8a77
Nikhil Gowda <nikhilgowda@outlook.com>
2019-01-31 21:17:46 +0530

    Commiting Dom Testing for Movie Lists Tasks

---
+++ movielists/src/Components/MovieLists/MovieLists.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Enzyme, {shallow, mount, childAt} from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import Movies from './MovieLists';
+
+describe('<Movies>',() => {
+    it('Should render a Div Dom element', ()=> {
+        const wrapper = shallow(<Movies />);
+        expect(wrapper.find(<div/>)).to.have.lengthOf(0);
+            });
+
+            it('renders an div with class `container`', () => {
+                const wrapper = shallow(<Movies />);
+                expect(wrapper.find('container')).to.have.lengthOf(0);
+              });
+
+              it('renders an div with class `row`', () => {
+                const wrapper = shallow(<Movies />);
+                expect(wrapper.find('row')).to.have.lengthOf(0);
+              });
+
+    it('Should render a table Dom element', ()=> {
+const wrapper = shallow(<Movies />);
+expect(wrapper.find(<table/>));
+expect(wrapper.find(<tr/>));
+expect(wrapper.find(<th/>));
+expect(wrapper.find(<td/>));
+    });
+
+    it('allows us to set props', () => {
+        const wrapper = mount( <table className="table table-striped" />);
+        expect(wrapper.props().className).to.equal('table table-striped');
+        wrapper.setProps({ className: 'foo' });
+        expect(wrapper.props().className).to.equal('foo');
+      });
+});
